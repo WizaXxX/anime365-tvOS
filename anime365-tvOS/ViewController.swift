4e2dd8573458fdd6b03d9d11413fc744230b0fce
WizaXxX <wizax0@gmail.com>
2022-08-12 18:17:00 +0300

    init

---
+++ anime365-tvOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  anime365-tvOS
+//
+//  Created by Илья Козырев on 12.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
